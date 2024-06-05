6ed4dabf51d78ac9ef7663e89b8e8186871724d8
Danileyko <119929429+Zanoza20@users.noreply.github.com>
2024-06-05 15:47:42 +0300

    Update TudoCard.js

--- диплом/js/TudoCard.js
+++ диплом/js/TudoCard.js
@@ -1,110 +1,137 @@
-// Объявляем переменные
-let goods = [];
-let cart = {};
+$(document).ready(function() {
+    var loginButton = $("#loginButton");
+    var registerButton = $("#registerButton");
+    var menuLogin = $("#menuLogin");
+    var menuRegister = $("#menuRegister");
+    var userRules = $("#userRules");
+    var userRulesLink = $("#userRulesLink");
+    var itemDetails = $("#itemDetails");
+    var cartIcon = $("#cartIcon");
+    var cartMenu = $("#cartMenu");
+    var cartItems = $("#cartItems");
+    var totalPrice = $("#totalPrice");
+    var orderButton = $("#orderButton");
 
-// Загружаем данные о товарах из JSON файла
-$.getJSON('json/goods.json', function (data) {
-    goods = data;
-    showGoods(goods);
-});
+    var cart = {};
 
-// Отображаем список товаров на странице
-function showGoods(goods) {
-    let out = '';
-    for (let key in goods) {
-        out += `<div class="good">`;
-        out += `<img src="${goods[key].image}" alt="${goods[key].name}">`;
-        out += `<h3>${goods[key].name}</h3>`;
-        out += `<p>Ціна: ${goods[key].price} ₴</p>`;
-        out += `<button class="add-to-cart" data-id="${key}">До кошику</button>`;
-        out += `</div>`;
+    loginButton.click(function() {
+        menuLogin.show();
+    });
+
+    registerButton.click(function() {
+        menuRegister.show();
+    });
+
+    $(".cancelbtn").click(function() {
+        menuLogin.hide();
+        menuRegister.hide();
+        userRules.hide();
+        itemDetails.hide();
+        cartMenu.hide();
+    });
+
+    userRulesLink.click(function(event) {
+        event.preventDefault();
+        userRules.show();
+    });
+
+    // Завантаження товарів
+    $.getJSON("goods.json", function(data) {
+        var goodsContainer = $("#goods");
+        $.each(data, function(key, value) {
+            goodsContainer.append(
+                '<div class="single-goods">' +
+                '<img src="' + value.image + '" alt="' + value.name + '">' +
+                '<h3 class="item-name" data-key="' + key + '">' + value.name + '</h3>' +
+                '<div class="price">₴' + value.cost + '</div>' +
+                '<button class="add-to-cart" data-key="' + key + '">Додати до кошика</button>' +
+                '</div>'
+            );
+        });
+
+        // Показувати деталі товару при натисканні на назву
+        $(".item-name").click(function() {
+            var key = $(this).data("key");
+            var item = data[key];
+
+            $("#itemImage").attr("src", item.image);
+            $("#itemName").text(item.name);
+            $("#itemDescription").text(item.descreption);
+            $("#brand").text(item.brand || "Невідомий");
+            $("#gpuManufacturer").text(item.gpuManufacturer || "Невідомий");
+            $("#graphicChip").text(item.graphicChip || "Невідомий");
+            $("#memorySize").text(item.memorySize || "Невідомий");
+            $("#memoryType").text(item.memoryType || "Невідомий");
+            $("#purpose").text(item.purpose || "Невідомий");
+            $("#coolingType").text(item.coolingType || "Невідомий");
+
+            itemDetails.show();
+        });
+
+        // Додавання товару до кошика
+        $(".add-to-cart").click(function() {
+            var key = $(this).data("key");
+            if (cart[key]) {
+                cart[key].quantity += 1;
+            } else {
+                cart[key] = {
+                    ...data[key],
+                    quantity: 1
+                };
+            }
+            updateCart();
+        });
+    });
+
+    // Пошук товару
+    $("#searchField").on("input", function() {
+        var searchTerm = $(this).val().toLowerCase();
+        $(".single-goods").each(function() {
+            var itemName = $(this).find(".item-name").text().toLowerCase();
+            if (itemName.includes(searchTerm)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+
+    // Відкрити кошик
+    cartIcon.click(function() {
+        cartMenu.toggle();
+        updateCart();
+    });
+
+    // Оновлення кошика
+    function updateCart() {
+        cartItems.empty();
+        var total = 0;
+        $.each(cart, function(key, item) {
+            var itemTotal = item.cost * item.quantity;
+            total += itemTotal;
+            cartItems.append(
+                '<li>' +
+                '<img src="' + item.image + '" alt="' + item.name + '">' +
+                item.name + ' x ' + item.quantity + ' - ₴' + itemTotal +
+                ' <button class="remove-from-cart" data-key="' + key + '">Видалити</button>' +
+                '</li>'
+            );
+        });
+        totalPrice.text('₴' + total);
+
+        // Видалення товару з кошика
+        $(".remove-from-cart").click(function() {
+            var key = $(this).data("key");
+            delete cart[key];
+            updateCart();
+        });
     }
-    $('#goods').html(out);
-}
 
-// Добавляем товар в корзину
-$('body').on('click', '.add-to-cart', function () {
-    let id = $(this).attr('data-id');
-    if (cart[id]) {
-        cart[id].qty++;
-    } else {
-        cart[id] = {
-            name: goods[id].name,
-            price: goods[id].price,
-            image: goods[id].image,
-            qty: 1
-        };
-    }
-    showCart();
-});
-
-// Отображаем содержимое корзины
-function showCart() {
-    let out = '';
-    let total = 0;
-    for (let key in cart) {
-        out += `<div class="cart-item">`;
-        out += `<img src="${cart[key].image}" alt="${cart[key].name}">`;
-        out += `<p>${cart[key].name}</p>`;
-        out += `<p>Ціна: ${cart[key].price} ₴</p>`;
-        out += `<p>Кількість: ${cart[key].qty}</p>`;
-        out += `<button class="delete-from-cart" data-id="${key}">&times;</button>`;
-        out += `</div>`;
-        total += cart[key].price * cart[key].qty;
-    }
-    $('#cartItems').html(out);
-    $('#totalPrice').html('₴' + total);
-}
-
-// Удаляем товар из корзины
-$('body').on('click', '.delete-from-cart', function () {
-    let id = $(this).attr('data-id');
-    if (cart[id].qty > 1) {
-        cart[id].qty--;
-    } else {
-        delete cart[id];
-    }
-    showCart();
-});
-
-// Очищаем корзину и закрываем её
-$('#clearCart').click(function () {
-    cart = {};
-    $('#cartMenu').hide();
-    showCart();
-});
-
-// Отправляем заказ
-function placeOrder() {
-    alert('Ваше замовлення прийнято!');
-    cart = {};
-    $('#cartMenu').hide();
-    showCart();
-}
-
-// Отображение модального окна корзины
-$('#cartIcon').click(function () {
-    showCart();
-    $('#cartMenu').show();
-});
-
-// Отображение модального окна входа
-$('#loginButton').click(function () {
-    $('#menuLogin').show();
-});
-
-// Отображение модального окна регистрации
-$('#registerButton').click(function () {
-    $('#menuRegister').show();
-});
-
-// Показать правила пользования
-$('#userRulesLink').click(function (event) {
-    event.preventDefault();
-    $('#userRules').show();
-});
-
-// Закрытие модальных окон
-$('.close').click(function () {
-    $(this).closest('.modal').hide();
+    // Оформлення замовлення
+    orderButton.click(function() {
+        alert("Замовлення оформлено! Сумарна ціна: " + totalPrice.text());
+        cart = {};
+        updateCart();
+        cartMenu.hide();
+    });
 });
