c65ff17b9ee1b8e30fbfc6ea4b8131c54df1a7e9
Danileyko <119929429+Zanoza20@users.noreply.github.com>
2024-06-12 12:21:10 +0300

    Update script.js

--- диплом/js/script.js
+++ диплом/js/script.js
@@ -22,11 +22,6 @@ $(document).ready(function () {
     var closeCartButton = $("#closeCartButton");
     var adminJournalIcon = $("#adminJournalIcon");
 
-    // Нові іконки дій
-    var addIcon = $("#addIcon");
-    var editIcon = $("#editIcon");
-    var deleteIcon = $("#deleteIcon");
-
     var cart = {};
     var goods = {};
     var currentUser = null;
@@ -177,11 +172,8 @@ $(document).ready(function () {
                 '</div>'
             );
         });
-        totalPrice.text('Загальна сума: ₴' + total);
-        setupRemoveFromCartButtons();
-    }
+        totalPrice.text("Загальна сума: ₴" + total);
 
-    function setupRemoveFromCartButtons() {
         $(".remove-from-cart").click(function () {
             var key = $(this).data("key");
             delete cart[key];
@@ -190,11 +182,11 @@ $(document).ready(function () {
     }
 
     cartIcon.click(function () {
-        if (!currentUser) {
-            alert("Ви повинні увійти в акаунт, щоб переглянути кошик.");
-            return;
-        }
         cartMenu.show();
+    });
+
+    closeCartButton.click(function () {
+        cartMenu.hide();
     });
 
     clearCartButton.click(function () {
@@ -203,83 +195,78 @@ $(document).ready(function () {
     });
 
     orderButton.click(function () {
-        alert("Ваше замовлення оформлено.");
-        cart = {};
-        updateCart();
-        cartMenu.hide();
+        if (Object.keys(cart).length === 0) {
+            alert("Ваш кошик порожній!");
+        } else {
+            alert("Замовлення успішно оформлене!");
+            cart = {};
+            updateCart();
+            cartMenu.hide();
+        }
     });
 
-    closeCartButton.click(function () {
-        cartMenu.hide();
+    $("#menuRegister form").submit(function (event) {
+        event.preventDefault();
+        var phone = $("#phoneRegister").val();
+        var email = $("#emailRegister").val();
+        var password = $("#pswRegister").val();
+
+        var users = JSON.parse(localStorage.getItem('users')) || [];
+        var userExists = users.some(user => user.phone === phone);
+
+        if (userExists) {
+            alert('Користувач з таким номером телефону вже існує');
+        } else {
+            users.push({ phone, email, password });
+            localStorage.setItem('users', JSON.stringify(users));
+            alert('Реєстрація пройшла успішно');
+            menuRegister.hide();
+        }
     });
 
-    logoutButton.click(function () {
-        currentUser = null;
-        profileIcon.hide();
-        loginButton.show();
-        registerButton.show();
-        adminJournalIcon.hide();
-        addIcon.hide();
-        editIcon.hide();
-        deleteIcon.hide();
-        alert("Ви вийшли з акаунту.");
-    });
-
-    // Перевірка входу адміністратора
-    $("#menuLogin form").on("submit", function (event) {
+    $("#menuLogin form").submit(function (event) {
         event.preventDefault();
         var phone = $("#phoneLogin").val();
         var password = $("#pswLogin").val();
 
         if (phone === actors.Admin.phone && password === actors.Admin.password) {
-            currentUser = actors.Admin;
-            profileIcon.show();
-            loginButton.hide();
-            registerButton.hide();
+            // Вхід як адміністратор
             adminJournalIcon.show();
-            addIcon.show();
-            editIcon.show();
-            deleteIcon.show();
+            alert('Вхід як адміністратор успішний');
             menuLogin.hide();
-            alert("Ви увійшли як адміністратор.");
-        } else {
-            alert("Невірний номер телефону або пароль.");
-        }
-    });
-
-    // Реєстрація нового користувача
-    $("#menuRegister form").on("submit", function (event) {
-        event.preventDefault();
-        var newUser = {
-            phone: $("#phoneRegister").val(),
-            email: $("#emailRegister").val(),
-            password: $("#pswRegister").val()
-        };
-
-        actors.users.push(newUser);
-        alert("Ви успішно зареєструвались. Тепер ви можете увійти.");
-        menuRegister.hide();
-    });
-
-    // Перевірка входу користувача
-    $("#menuLogin form").on("submit", function (event) {
-        event.preventDefault();
-        var phone = $("#phoneLogin").val();
-        var password = $("#pswLogin").val();
-
-        var user = actors.users.find(function (user) {
-            return user.phone === phone && user.password === password;
-        });
-
-        if (user) {
-            currentUser = user;
             profileIcon.show();
             loginButton.hide();
             registerButton.hide();
-            menuLogin.hide();
-            alert("Ви увійшли в акаунт.");
+            currentUser = actors.Admin;
         } else {
-            alert("Невірний номер телефону або пароль.");
+            var users = JSON.parse(localStorage.getItem('users')) || [];
+            var user = users.find(user => user.phone === phone && user.password === password);
+
+            if (user) {
+                alert('Вхід успішний');
+                menuLogin.hide();
+                profileIcon.show();
+                loginButton.hide();
+                registerButton.hide();
+                currentUser = user;
+                $("#profileEmail").text(user.email);
+                $("#profilePhone").text(user.phone);
+            } else {
+                alert('Невірний номер телефону або пароль');
+            }
         }
+    });
+
+    profileIcon.click(function () {
+        profileModal.show();
+    });
+
+    logoutButton.click(function () {
+        currentUser = null;
+        profileModal.hide();
+        profileIcon.hide();
+        adminJournalIcon.hide();
+        loginButton.show();
+        registerButton.show();
     });
 });
