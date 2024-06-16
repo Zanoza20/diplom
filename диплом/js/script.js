7df4068f0ab086b58458f005497004d68b1a57c2
Danileyko <119929429+Zanoza20@users.noreply.github.com>
2024-06-16 12:40:41 +0300

    Update script.js

--- диплом/js/script.js
+++ диплом/js/script.js
@@ -487,11 +487,6 @@ $(document).ready(function () {
             ordersList.append("<p>Час: " + order.time + " - Телефон: " + order.phone + " - Email: " + order.email + " - Товари: " + order.items + " - Сума: ₴" + order.total + "</p>");
         });
     }
- $("#helpButton").click(function () {
-        $("#helpModal").show();
-    });
 
-    $("#closeHelpModalButton").click(function () {
-        $("#helpModal").hide();
     displayGoods(goods);
 });
